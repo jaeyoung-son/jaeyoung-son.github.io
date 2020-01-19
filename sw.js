84919d1bded431853034410393baa201bc1bb918
손재영 <sonjaeyeong@sonjaeyeong-ui-MacBookPro.local>
2020-01-19 15:02:04 +0900

    Updates

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ self.__precacheManifest = [
     "url": "styles-cad9bf75f8994da31389.js"
   },
   {
-    "url": "commons-4237c50560f02fc88589.js"
+    "url": "commons-429bda5c2c5a0f382b85.js"
   },
   {
     "url": "app-eff881f9d74ef7b4e7e4.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "805b6de56d50a63a5cf62ffb7ca4936d"
+    "revision": "384fd19a80c8e6286ea2080fb0d37598"
   },
   {
     "url": "manifest.json",
